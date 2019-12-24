35850e6de6147bfd73b6525c5a230115007d2eaa
PavloVoitenko <pavlo.voitenko@hotmail.com>
2019-12-24 13:24:33 +0200

    Remove redundant files and update dependency

--- MetadataApi.Model/Entities/MetaEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using MetadataApi.Model.Entities.Abstractions;
-
-namespace MetadataApi.Model.Entities
-{
-    public class MetaEntity : MetaEntityBase
-    {
-        public bool ShowHeader { get; set; }
-    }
-}
